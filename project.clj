c22ddd000b651a682134427bea39a32ed566b457
Kenneth Leung <ccllnekk@gmail.com>
2017-01-15 01:44:44 -0800

    kl

---
+++ project.clj
@@ -0,0 +1,43 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+(defproject io.czlab/wabbit-mock "0.1.0"
+
+  :license {:url "http://www.eclipse.org/legal/epl-v10.html"
+            :name "Eclipse Public License"}
+
+  :description ""
+  :url "https://github.com/llnek/wabbit-mock"
+
+  :dependencies [[org.apache.geronimo.specs/geronimo-jms_1.1_spec "1.1.1"]
+                 [com.sun.mail/javax.mail "1.5.6"]
+                 [io.czlab/xlib "0.1.0"]]
+
+  :plugins [[lein-codox "0.10.2"]
+            [lein-pprint "1.1.2"]]
+
+  :profiles {:provided {:dependencies
+                        [[net.mikera/cljunit "0.6.0" :scope "test"]
+                         [junit/junit "4.12" :scope "test"]
+                         [org.clojure/clojure "1.8.0" :scope "provided"]
+                         [codox/codox "0.10.2" :scope "provided"]]}
+             :uberjar {:aot :all}}
+
+  :global-vars {*warn-on-reflection* true}
+  :target-path "out/%s"
+  :aot :all
+  ;;:jar-exclusions [#"(?:^|/).svn/"]
+  :coordinate! "czlab/wabbit/mock"
+  :omit-source true
+
+  :java-source-paths ["src/main/java"]
+  :source-paths ["src/main/clojure"]
+  ;;:test-paths ["src/test/clojure"]
+  ;;:resource-paths ["src/main/resources"]
+
+  :jvm-opts ["-Dlog4j.configurationFile=file:attic/log4j2.xml"]
+  :javac-options ["-source" "8"
+                  "-Xlint:unchecked" "-Xlint:-options" "-Xlint:deprecation"])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;EOF
+
