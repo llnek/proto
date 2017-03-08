73938616787bbbb75dd7fc4b3254d0be364c0685
Kenneth Leung <ccllnekk@gmail.com>
2017-03-07 22:57:43 -0800

    kl

--- project.clj
+++ project.clj
@@ -12,26 +12,30 @@
                  [com.sun.mail/javax.mail "1.5.6"]
                  [io.czlab/basal "1.0.0"]]
 
-  :plugins [[lein-codox "0.10.2"]
-            [lein-pprint "1.1.2"]]
+  :plugins [[cider/cider-nrepl "0.14.0"]
+            [lein-javadoc "0.3.0"]
+            [lein-codox "0.10.3"]
+            [lein-cprint "1.2.0"]]
 
   :profiles {:provided {:dependencies
                         [[org.clojure/clojure "1.8.0" :scope "provided"]
                          [net.mikera/cljunit "0.6.0" :scope "test"]
-                         [junit/junit "4.12" :scope "test"]
-                         [codox/codox "0.10.2" :scope "provided"]]}
+                         [junit/junit "4.12" :scope "test"]]}
              :uberjar {:aot :all}}
+
+  :javadoc-opts {:package-names ["czlab.proto"]
+                 :output-dir "docs"}
 
   :global-vars {*warn-on-reflection* true}
   :target-path "out/%s"
   :aot :all
-  ;;:jar-exclusions [#"(?:^|/).svn/"]
+
   :coordinate! "czlab"
   :omit-source true
 
-  :java-source-paths ["src/main/java"]
+  :java-source-paths ["src/main/java" "src/test/java"]
   :source-paths ["src/main/clojure"]
-  ;;:test-paths ["src/test/clojure"]
+  :test-paths ["src/test/clojure"]
   ;;:resource-paths ["src/main/resources"]
 
   :jvm-opts ["-Dlog4j.configurationFile=file:attic/log4j2.xml"]
